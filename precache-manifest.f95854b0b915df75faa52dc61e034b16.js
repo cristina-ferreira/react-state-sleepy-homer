a2c2e9f076ebea931413b8e7b19296722e869a87
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-24 15:18:13 +0100

    Updates

---
+++ precache-manifest.f95854b0b915df75faa52dc61e034b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611f9bc192930a5d1fe4c1b3f893a005",
+    "url": "/react-state-sleepy-homer/index.html"
+  },
+  {
+    "revision": "dbfde5dd86b7ba1801cd",
+    "url": "/react-state-sleepy-homer/static/css/main.af0a5a8e.chunk.css"
+  },
+  {
+    "revision": "61477f81220081e1df57",
+    "url": "/react-state-sleepy-homer/static/js/2.901827b4.chunk.js"
+  },
+  {
+    "revision": "dbfde5dd86b7ba1801cd",
+    "url": "/react-state-sleepy-homer/static/js/main.0fcadedd.chunk.js"
+  },
+  {
+    "revision": "5781ccc4a073793d275b",
+    "url": "/react-state-sleepy-homer/static/js/runtime~main.2c9bdf70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-state-sleepy-homer/static/media/logo.5d5d9eef.svg"
+  }
+]);
